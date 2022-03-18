41f04f9b3b6930f2867e7f5bab32c525a64ea23d
JelleDekkers <jelle.dekkers@hotmail.com>
2022-03-18 14:57:39 +0100

    Implemented dish food stacking

--- Assets/Physics/Physics/Tools/Tool.cs
+++ Assets/Physics/Physics/Tools/Tool.cs
@@ -226,6 +226,9 @@ namespace PhysicsCharacter
 				heldHandles = 0;
 			heldHandles++;
 			rigidBody.useGravity = false;
+
+			if (TryGetComponent(out Photon.Pun.PhotonView photonView))
+				photonView.TransferOwnership(Photon.Pun.PhotonNetwork.LocalPlayer);
 		}
 
 		protected virtual void OnReleasedCallback(Hand hand, ToolHandle toolHandle)
@@ -239,6 +242,9 @@ namespace PhysicsCharacter
 			toolHandle.transform.parent = transform;
 			toolHandle.transform.localPosition = toolHandle.localTransformMirror.localPosition;
 			toolHandle.transform.localRotation = toolHandle.localTransformMirror.localRotation;
+
+			if (TryGetComponent(out Photon.Pun.PhotonView photonView))
+				photonView.TransferOwnership(-1);
 		}
 
 		protected virtual void MoveUsingPhysics()
