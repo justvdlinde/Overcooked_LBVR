07eeaea05c6ae22c7e7b57c1bb22b47fd1b625b3
Joas Kleine <Joas@knucklehead-studios.com>
2022-03-18 17:53:27 +0100

    Added sound to dispenser(plop sound for now, dispenser sound too intense with headset)

--- Assets/Scripts/OvercookedLBVR/Dispenser.cs
+++ Assets/Scripts/OvercookedLBVR/Dispenser.cs
@@ -8,16 +8,22 @@ public class Dispenser : MonoBehaviour
     [SerializeField] private Transform spawnPoint = null;
     [SerializeField] private float forceAmount = 1;
     [SerializeField] private ForceMode forceMode = ForceMode.Force;
+    [SerializeField] private AudioSource AudioSource;
+
 
     public void DispenseObject(Rigidbody obj)
     {
         GameObject g = PhotonNetwork.Instantiate(prefab.name, spawnPoint.position, spawnPoint.rotation);
+        AudioSource.Play();
         //instance.AddForce(instance.transform.forward * forceAmount, forceMode);
     }
 
     [Button]
     public void DispenseObject()
-    {
+    { 
         DispenseObject(prefab);
+
     }
 }
+
+
