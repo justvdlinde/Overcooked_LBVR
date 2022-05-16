f89489075799949162c713a192cb222ea00a9e45
Vincentmc <Vincentmc99@hotmail.com>
2022-05-16 14:44:43 +0200

    RW merge

--- Assets/Scripts/Player/Pawn/PlayerPawnHandAnimatorRW.cs
+++ Assets/Scripts/Player/Pawn/PlayerPawnHandAnimatorRW.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using PhysicsCharacter;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -25,7 +26,97 @@ public class PlayerPawnHandAnimatorRW : MonoBehaviourPun, IPunObservable
 
 	private const string animatorString = "FingerFloat";
 
-    private void Start()
+	[SerializeField] private PhysicsPickupManager pickupManager = null;
+
+	[Header("remote only vars")]
+	[SerializeField] private Transform handGraphics = null;
+	[SerializeField] private Transform pickupPivot = null;
+	private Vector3 originalLocalPos;
+	private Quaternion originalLocalRot;
+	private Vector3 originalLocalScale;
+
+	public bool isRemoteClient = false;
+	private Transform followTarget = null;
+
+	private void Awake()
+	{
+		if(isRemoteClient && handGraphics != null)
+		{
+			originalLocalPos = handGraphics.localPosition;
+			originalLocalRot = handGraphics.localRotation;
+			originalLocalScale = handGraphics.localScale;
+		}
+	}
+
+	private void OnEnable()
+	{
+		if(!isRemoteClient && pickupManager != null)
+		{
+			pickupManager.OnPickupEvent += OnPickupEvent;
+			pickupManager.OnDropEvent += OnDropEvent;
+		}
+	}
+
+	private void OnDisable()
+	{
+		if(!isRemoteClient && pickupManager != null)
+		{
+			pickupManager.OnPickupEvent -= OnPickupEvent;
+			pickupManager.OnDropEvent -= OnDropEvent;
+		}
+	}
+
+	private void OnPickupEvent(Hand hand, Tool tool)
+	{
+		Debug.Log($"picked up photonview ID {tool.RootPhotonView.ViewID} with {hand.ToString()} hand");
+		photonView.RPC(nameof(OnPickupEventRPC), RpcTarget.Others, tool.RootPhotonView.ViewID);
+
+	}
+
+	[PunRPC]
+	public void OnPickupEventRPC(int viewID)
+	{
+		Tool tool = PhotonView.Find(viewID).GetComponentInChildren<Tool>();
+		Debug.Log($"photonView ID {photonView.ViewID} grabbed tool with ID {viewID}");
+
+		if (!isRemoteClient)
+			return;
+
+		handGraphics.transform.parent = tool.transform;
+		followTarget = tool.GetFollowPosAndRot(hand);
+		Quaternion rot = tool.GetFollowQuat(hand);
+		handGraphics.transform.position = followTarget.transform.position;
+		handGraphics.transform.position += pickupPivot.localPosition;
+		// add some form of offset from handle
+		handGraphics.transform.forward = pickupPivot.forward;
+		//handGraphics.transform.rotation = followTarget.transform.rotation * tool.GetFollowQuat(hand);// followTarget.transform.rotation;// * rot;
+
+	}
+
+
+	private void OnDropEvent(Hand hand, Tool tool)
+	{
+		Debug.Log($"dropped photonview ID {tool.RootPhotonView.ViewID} with {hand.ToString()} hand");
+		photonView.RPC(nameof(OnDropEventRPC), RpcTarget.Others, tool.RootPhotonView.ViewID);
+	}
+
+	[PunRPC]
+	public void OnDropEventRPC(int viewID)
+	{
+		Tool tool = PhotonView.Find(viewID).GetComponentInChildren<Tool>();
+		Debug.Log($"photonView ID {photonView.ViewID} dropped tool with ID {viewID}");
+
+		if (!isRemoteClient)
+			return;
+
+		handGraphics.transform.parent = transform;
+		followTarget = null;
+		handGraphics.localPosition = originalLocalPos;
+		handGraphics.localRotation = originalLocalRot;
+		handGraphics.localScale = originalLocalScale;
+	}
+
+	private void Start()
     {
 		// Because local and remote prefabs are different, the view IDs need to be setup manually:
 		int increment = hand == Hand.Left ? PhotonIdentifiers.LeftHand : PhotonIdentifiers.RightHand;
