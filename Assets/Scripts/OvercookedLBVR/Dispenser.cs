da54f33e59ef21e1c9f7a866286a40d9b100d656
KHS-Jelle <jelle.dekkers@hotmail.com>
2022-06-02 14:05:35 +0200

    Updated dispenser to use 'shoot' out object using physics

--- Assets/Scripts/OvercookedLBVR/Dispenser.cs
+++ Assets/Scripts/OvercookedLBVR/Dispenser.cs
@@ -1,6 +1,7 @@
 using Photon.Pun;
 using System;
 using UnityEngine;
+using Utils.Core;
 using Utils.Core.Attributes;
 
 [SelectionBase]
@@ -8,9 +9,13 @@ public class Dispenser : MonoBehaviourPun
 {
     [SerializeField] private Rigidbody prefab = null;
     [SerializeField] private Transform spawnPoint = null;
+    [SerializeField] private AudioSource AudioSource;
+
+    [Header("Force")]
+    [SerializeField] private bool instantiateWithForce = false;
+    [SerializeField] private Vector3 forceDirection;
     [SerializeField] private float forceAmount = 1;
     [SerializeField] private ForceMode forceMode = ForceMode.Force;
-    [SerializeField] private AudioSource AudioSource;
 
     private DateTime timeLastAcitvated;
     private const float minSecondsBetweenDispense = 0.5f;
@@ -41,6 +46,19 @@ public class Dispenser : MonoBehaviourPun
         // TODO: place this on prefabs, as this won't work for players joining late
         instance.AddComponent(typeof(DestroyOnGameStart));
         AudioSource.Play();
+        if(instantiateWithForce)
+        {
+            if(instance.TryGetComponent(out Rigidbody rigidbody))
+            {
+                rigidbody.AddForce(forceDirection * forceAmount, forceMode);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(instantiateWithForce)
+            GizmosUtility.DrawArrow(spawnPoint.position, forceDirection);
     }
 }
 
